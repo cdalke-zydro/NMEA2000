84132e080d8684771a82b349a2b6eadb789b9752
Chris Dalke <chris@zydromarine.com>
2025-07-11 12:12:06 -0400

    wip: throttle control PGNs

--- src/N2kZydro.h
+++ src/N2kZydro.h
@@ -47,11 +47,13 @@
 #include "N2kTypes.h"
 #include <stdint.h>
 
+// Enumeration of different device types
 enum tN2kZydroDeviceModel {
     tN2kZydroDeviceModel_invalid=0,
     tN2kZydroDeviceModel_estop=1
 };
 
+// Enumeration of health status of a device
 enum tN2kZydroDeviceHealth {
     tN2kZydroDeviceHealth_invalid=0,
     tN2kZydroDeviceHealth_pending=1,
@@ -60,8 +62,16 @@ enum tN2kZydroDeviceHealth {
     tN2kZydroDeviceHealth_emergencyStop=4
 };
 
+// Enumeration of throttle controller modes
+enum tN2kZydroThrottleSetpointMode {
+    tN2kZydroThrottleSetpointMode_invalid=0,
+    tN2kZydroThrottleSetpointMode_idle=1,
+    tN2kZydroThrottleSetpointMode_throttlePercentage=2,
+    tN2kZydroThrottleSetpointMode_rpm=3
+}
+
 /**************************************************************************
- * \brief Setting up PGN 65280 for Zydro "Product Heartbeat"
+ * \brief PGN 65280: Zydro "Product Heartbeat"
  * 
  * This message is broadcast periodically by all Zydro products to provide
  * a heartbeat used for auto-detection purposes.
@@ -76,24 +86,38 @@ enum tN2kZydroDeviceHealth {
  * \param Health          Health status for the device, from a Zydro internal enumeration.
  */
 void SetN2kPGN65280(tN2kMsg &N2kMsg, unsigned char SID, tN2kZydroDeviceModel ModelID, tN2kZydroDeviceHealth Health);
+bool ParseN2kPGN65280(const tN2kMsg &N2kMsg, unsigned char &SID, tN2kZydroDeviceModel &ModelID, tN2kZydroDeviceHealth &Health);
 
 /**************************************************************************
- * \brief Parsing PGN 65280 for Zydro "Product Heartbeat"
+ * \brief PGN 65281: Zydro "Throttle Control Setpoint"
  * 
- * This message is broadcast periodically by all Zydro products to provide
- * a heartbeat used for auto-detection purposes.
+ * This message is used to set a target for a throttle & shift controller.
  * 
  * \param N2kMsg          Reference to a N2kMsg Object, 
  *                        Output: NMEA2000 message ready to be send.
- * \param SID             Sequence ID. Normally you can just forget its value. See \ref secRefTermSID.
- * \param ModelID         Zydro-specific product model ID, from a Zydro internal enumeration.
- * \param Health          Health status for the device, from a Zydro internal enumeration.
- *
- * \return true     Parsing of PGN Message successful
- * \return false    Parsing of PGN Message aborted
- *
+ * \param ThrottleID      Integer ID for the throttle channel; typically 0 (all engines), 1 (port) or 2 (starboard).
+ * \param Mode            Setpoint mode, from a Zydro internal enumeration.
+ * \param Target          Target throttle value, as double. In throttle percentage mode, this is -1.0 to 1.0, in RPM mode this is an RPM.
+ * \param ShiftGears      Boolean, whether to also shift engine gear in tandem with throttle if available.
  */
-bool ParseN2kPGN65280(const tN2kMsg &N2kMsg, unsigned char &SID, tN2kZydroDeviceModel &ModelID, tN2kZydroDeviceHealth &Health);
+void SetN2kPGN65281(tN2kMsg &N2kMsg, unsigned char ThrottleID, tN2kZydroThrottleSetpointMode Mode, double Target, bool ShiftGears);
+bool ParseN2kPGN65281(const tN2kMsg &N2kMsg, unsigned char &ThrottleID, tN2kZydroThrottleSetpointMode &Mode, double &Target, bool &ShiftGears);
+
+/**************************************************************************
+ * \brief PGN 65282: Zydro "Throttle Control Status"
+ * 
+ * This message is used to broadcast the status of a throttle & shift controller.
+ * 
+ * \param N2kMsg          Reference to a N2kMsg Object, 
+ *                        Output: NMEA2000 message ready to be send.
+ * \param ThrottleID      Integer ID for the throttle channel; typically 0 (all engines), 1 (port) or 2 (starboard).
+ * \param Mode            Active setpoint mode, from a Zydro internal enumeration.
+ * \param TargetValue     Target setpoint value.
+ * \param CurrentValue    Current control value.
+ * \param CurrentGear     Current gear value (0 = disengaged, 1 = forward, 2 = reverse)
+ */
+void SetN2kPGN65282(tN2kMsg &N2kMsg, unsigned char ThrottleID, tN2kZydroThrottleSetpointMode Mode, double TargetValue, double CurrentValue, unsigned char CurrentGear);
+bool ParseN2kPGN65282(const tN2kMsg &N2kMsg, unsigned char &ThrottleID, tN2kZydroThrottleSetpointMode &Mode, double &TargetValue, double &CurrentValue, unsigned char &CurrentGear);
 
 /**************************************************************************/
 
